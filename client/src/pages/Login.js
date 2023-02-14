a0c78cdf2486b625ee50e1fc3e1b2f3bef4fa29d
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-02-13 21:28:52 -0800

    client side edits

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,7 +7,7 @@ function Login() {
     async function handleSubmit(e) {
         e.preventDefault();
         console.log('get data!')
-        navigate('/');
+        navigate('/book');
     }
 
     //use session cookies
@@ -15,14 +15,44 @@ function Login() {
     return (
         <div>
             <NavBar />
-            <form method="post">
+                <form method="post">
+                <h1> Log In</h1>
       <input type="text" name="u" placeholder="Username" required="required" />
         <input type="password" name="p" placeholder="Password" required="required" />
         <button
             type="submit"
-            class="btn btn-primary btn-block btn-large"
+            className="btn btn-primary btn-block btn-large"
             onClick={handleSubmit}
         >Login</button>
+    </form>
+
+                {/* <form method="post">
+                <h1> Create Account</h1>
+      <input type="text" name="u" placeholder="Username" required="required" />
+        <input type="password" name="p" placeholder="Password" required="required" />
+        <button
+            type="submit"
+            className="btn btn-primary btn-block btn-large"
+            onClick={handleSubmit}
+        >Create</button>
+    </form> */}
+
+    <form class="form signup-form">
+      <div class="form-group">
+        <label for="username-signup">Username:</label>
+        <input class="form-input" type="text" id="username-signup" />
+      </div>
+      <div class="form-group">
+        <label for="email-signup">Email:</label>
+        <input class="form-input" type="text" id="email-signup" />
+      </div>
+      <div class="form-group">
+        <label for="password-signup">Password:</label>
+        <input class="form-input" type="password" id="password-signup" />
+      </div>
+      <div class="form-group">
+        <button class="btn btn-primary" type="submit">signup</button>
+      </div>
     </form>
         </div>
     )
