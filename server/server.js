f9ccea72cce25a26da68a4bd40d9fdc9f2b3e4a0
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-02-13 21:25:58 -0800

    started on session cookies

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 const app = express();
 
